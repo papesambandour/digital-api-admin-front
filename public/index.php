d8386a19b9dea593626adb4eb467a2b70e800871
Moussa Ndour <moussa.ndour@intech.sn>
2023-03-09 03:46:25 +0100

    increase exec limit

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 ini_set('memory_limit', -1);
+ini_set('max_execution_time', 500);
 
 
 define('LARAVEL_START', microtime(true));
