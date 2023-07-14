628d6f40b4a7df89060c87e822bfe419d88f30b2
Moussa Ndour <moussa.ndour@intech.sn>
2023-07-14 07:28:21 +0000

    erorr message

--- resources/views/pages/transaction/transaction.blade.php
+++ resources/views/pages/transaction/transaction.blade.php
@@ -197,7 +197,12 @@
                                         <span class="{{status($transaction->{STATUS_TRX_NAME})}}">
                                         {{ $transaction->{STATUS_TRX_NAME} }}
                                         </span>
-
+                                        <details>
+                                            <summary>voir message</summary>
+                                            <p>
+                                                {{$transaction->error_message}}
+                                            </p>
+                                        </details>
                                     </td>
                                     <td>
                                         {{ $transaction->created_at }}
